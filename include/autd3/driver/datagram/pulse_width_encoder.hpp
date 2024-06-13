b2dbd7138d717340e3cddbd99375ac581b52a8f1
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-06-13 20:36:15 +0900

    fix `PulseWidthEncoder`

--- include/autd3/driver/datagram/pulse_width_encoder.hpp
+++ include/autd3/driver/datagram/pulse_width_encoder.hpp
@@ -1,8 +1,8 @@
 #pragma once
 
 #include <memory>
-#include <mutex>
 #include <optional>
+#include <shared_mutex>
 #include <unordered_map>
 
 #include "autd3/driver/datagram/tuple.hpp"
@@ -20,19 +20,27 @@ class PulseWidthEncoder final : public IntoDatagramTuple<PulseWidthEncoder>,
 
  public:
   AUTD3_API PulseWidthEncoder() : _f(std::nullopt) {}
-  AUTD3_API explicit PulseWidthEncoder(std::function<std::function<uint16_t(size_t)>(const geometry::Device&)> f)
-      : _f(std::move(f)), _mtx(std::make_shared<std::mutex>()) {
+  AUTD3_API explicit PulseWidthEncoder(std::function<std::function<uint16_t(size_t)>(const geometry::Device&)> f) : _f(std::move(f)) {
     _f_native = +[](const void* context, const native_methods::GeometryPtr geometry_ptr, const uint16_t dev_idx, const uint16_t i) -> uint16_t {
       auto* self = static_cast<PulseWidthEncoder*>(const_cast<void*>(context));
-      if (!self->_cache.contains(dev_idx)) {
-        const geometry::Device dev(dev_idx, AUTDDevice(geometry_ptr, dev_idx));
-        auto f = self->_f.value()(dev);
-        std::lock_guard lock(*self->_mtx);
-        self->_cache[dev_idx] = f;
+      bool contains;
+      {
+        std::shared_lock lock(self->_mtx);
+        contains = self->_cache.contains(dev_idx);
       }
-      return self->_cache[dev_idx](i);
+      if (contains) return self->_cache[dev_idx](i);
+      auto f = self->_f.value()(geometry::Device(dev_idx, AUTDDevice(geometry_ptr, dev_idx)));
+      const auto res = f(i);
+      {
+        std::lock_guard lock(self->_mtx);
+        self->_cache[dev_idx] = std::move(f);
+      }
+      return res;
     };
   }
+
+  PulseWidthEncoder(const PulseWidthEncoder& other) noexcept : _f(other._f), _f_native(other._f_native) {}        // LCOV_EXCL_LINE
+  PulseWidthEncoder(PulseWidthEncoder&& other) noexcept : _f(std::move(other._f)), _f_native(other._f_native) {}  // LCOV_EXCL_LINE
 
   AUTD3_API [[nodiscard]] native_methods::DatagramPtr ptr(const geometry::Geometry& geometry) const {
     return _f.has_value() ? AUTDDatagramPulseWidthEncoder(reinterpret_cast<const void*>(_f_native),
@@ -43,7 +51,7 @@ class PulseWidthEncoder final : public IntoDatagramTuple<PulseWidthEncoder>,
  private:
   std::optional<std::function<std::function<uint16_t(size_t)>(const geometry::Device&)>> _f;
   native_f _f_native = nullptr;
-  std::shared_ptr<std::mutex> _mtx;
+  std::shared_mutex _mtx;
   std::unordered_map<uint16_t, std::function<uint16_t(size_t)>> _cache;
 };
 
