a2b8c3d0026133090db69c514b8f0c2d9326271b
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-06-27 11:31:20 +0900

    fix test name

--- tests/driver/firmware/fpga/stm_sampling_config.cpp
+++ tests/driver/firmware/fpga/stm_sampling_config.cpp
@@ -2,7 +2,7 @@
 
 #include <autd3/driver/firmware/fpga/stm_sampling_config.hpp>
 
-TEST(DriverFirmwareFPGA, SamplingConfigFreq) {
+TEST(DriverFirmwareFPGA, STMSamplingConfigFreq) {
   const autd3::native_methods::STMSamplingConfigWrap f = autd3::driver::STMSamplingConfig::Freq(4000.0f * autd3::driver::Hz);
   ASSERT_EQ(f.tag, autd3::native_methods::STMSamplingConfigTag::Freq);
   ASSERT_EQ(f.value.freq, 4000.0f);
