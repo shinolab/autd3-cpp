e33a576b21208848dbb1d19c666b6472751bde77
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-08-07 16:33:41 +0900

    add tests for `Drive`

--- tests/driver/firmware/fpga/drive.cpp
+++ tests/driver/firmware/fpga/drive.cpp
@@ -2,4 +2,25 @@
 
 #include <autd3/driver/firmware/fpga/drive.hpp>
 
-TEST(DriverFirmwareFPGA, Drive) {}
+TEST(DriverFirmwareFPGA, Drive) {
+  {
+    const autd3::driver::Drive d(autd3::driver::Phase(0x90), autd3::driver::EmitIntensity(0x91));
+    ASSERT_EQ(0x90, d.phase.value());
+    ASSERT_EQ(0x91, d.intensity.value());
+  }
+  {
+    const autd3::driver::Drive d(autd3::driver::EmitIntensity(0x91), autd3::driver::Phase(0x90));
+    ASSERT_EQ(0x90, d.phase.value());
+    ASSERT_EQ(0x91, d.intensity.value());
+  }
+  {
+    const autd3::driver::Drive d(autd3::driver::Phase(0x90));
+    ASSERT_EQ(0x90, d.phase.value());
+    ASSERT_EQ(0xFF, d.intensity.value());
+  }
+  {
+    const autd3::driver::Drive d(autd3::driver::EmitIntensity(0x90));
+    ASSERT_EQ(0x00, d.phase.value());
+    ASSERT_EQ(0x90, d.intensity.value());
+  }
+}
