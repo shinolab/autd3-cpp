58db2adbe81f6acf87f4f9c241f9592d9b5be635
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2024-10-10 08:22:29 +0900

    update tests

--- tests/driver/datagram/with_parallel_threshold.cpp
+++ tests/driver/datagram/with_parallel_threshold.cpp
@@ -11,13 +11,13 @@ TEST(DriverDatagram, WithParallelThreshold) {
                   std::vector{autd3::driver::AUTD3(autd3::driver::Vector3::Zero()), autd3::driver::AUTD3(autd3::driver::Vector3::Zero())})
                   .with_parallel_threshold(0)
                   .open(autd3::link::Audit::builder());
-  ASSERT_EQ(0xFFFF, autd.link().last_parallel_threshold());
+  ASSERT_EQ(0xFFFFFFFFFFFFFFFF, autd.link().last_parallel_threshold());
 
   autd.send(autd3::gain::Null());
   ASSERT_EQ(0, autd.link().last_parallel_threshold());
 
   autd.send(autd3::modulation::Static());
-  ASSERT_EQ(0xFFFF, autd.link().last_parallel_threshold());
+  ASSERT_EQ(0xFFFFFFFFFFFFFFFF, autd.link().last_parallel_threshold());
 
   autd.send(autd3::modulation::Static().with_parallel_threshold(10));
   ASSERT_EQ(10, autd.link().last_parallel_threshold());
--- tests/modulation/sine.cpp
+++ tests/modulation/sine.cpp
@@ -10,19 +10,21 @@ TEST(Modulation, Sine) {
   {
     const auto m = autd3::modulation::Sine(150 * autd3::driver::Hz)
                        .with_intensity(std::numeric_limits<uint8_t>::max() / 2)
-                       .with_offset(std::numeric_limits<uint8_t>::max() / 4)
+                       .with_offset(std::numeric_limits<uint8_t>::max() / 2)
                        .with_phase(autd3::driver::pi / 2 * autd3::driver::rad);
     ASSERT_EQ(std::numeric_limits<uint8_t>::max() / 2, m.intensity());
-    ASSERT_EQ(std::numeric_limits<uint8_t>::max() / 4, m.offset());
+    ASSERT_EQ(std::numeric_limits<uint8_t>::max() / 2, m.offset());
     ASSERT_EQ(autd3::driver::pi / 2 * autd3::driver::rad, m.phase());
     ASSERT_EQ(10, m.sampling_config().division());
     autd.send(m);
 
     for (auto& dev : autd.geometry()) {
       auto mod = autd.link().modulation(dev.idx(), autd3::native_methods::Segment::S0);
-      std::vector<uint8_t> mod_expect{127, 125, 120, 111, 100, 87, 73, 58, 43, 30, 18, 9, 3, 0, 0, 4, 12, 22, 34, 48, 63, 78, 92,  104, 114, 122, 126,
-                                      126, 123, 117, 108, 96,  83, 68, 53, 39, 26, 15, 6, 1, 0, 1, 6, 15, 26, 39, 53, 68, 83, 96,  108, 117, 123, 126,
-                                      126, 122, 114, 104, 92,  78, 63, 48, 34, 22, 12, 4, 0, 0, 3, 9, 18, 30, 43, 58, 73, 87, 100, 111, 120, 125};
+      std::vector<uint8_t> mod_expect{
+          127, 125, 120, 112, 101, 88, 73, 59, 44, 30, 19, 9, 3, 0, 1, 5, 12, 22, 35, 49, 64, 78, 92,  105, 115, 122, 126,
+          127, 124, 118, 108, 97,  83, 68, 54, 39, 26, 15, 7, 2, 0, 2, 7, 15, 26, 39, 54, 68, 83, 97,  108, 118, 124, 127,
+          126, 122, 115, 105, 92,  78, 64, 49, 35, 22, 12, 5, 1, 0, 3, 9, 19, 30, 44, 59, 73, 88, 101, 112, 120, 125,
+      };
       ASSERT_TRUE(std::ranges::equal(mod, mod_expect, [](const auto& l, const auto& r) { return l == r; }));
       ASSERT_EQ(10, autd.link().modulation_freq_division(dev.idx(), autd3::native_methods::Segment::S0));
     }
@@ -45,8 +47,8 @@ TEST(Modulation, SineWithMode) {
 
   for (auto& dev : autd.geometry()) {
     auto mod = autd.link().modulation(dev.idx(), autd3::native_methods::Segment::S0);
-    std::vector<uint8_t> mod_expect{127, 156, 184, 209, 229, 244, 253, 254, 249, 237, 220, 197, 171, 142,
-                                    112, 83,  57,  34,  17,  5,   0,   1,   10,  25,  45,  70,  98};
+    std::vector<uint8_t> mod_expect{128, 157, 185, 209, 230, 245, 253, 255, 250, 238, 220, 198, 171, 142,
+                                    113, 84,  57,  35,  17,  5,   0,   2,   10,  25,  46,  70,  98};
     ASSERT_TRUE(std::ranges::equal(mod, mod_expect, [](const auto& l, const auto& r) { return l == r; }));
   }
 
