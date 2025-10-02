f98231975f61b8d596d5143ef277b5097c0a301a
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2025-10-02 10:57:59 +0900

    update `SwapSegment`

--- include/autd3/driver/datagram/segment.hpp
+++ include/autd3/driver/datagram/segment.hpp
@@ -6,51 +6,51 @@
 
 namespace autd3::driver {
 
-struct SwapSegment {
-  struct Gain final : Datagram, IntoDatagramTuple<Gain> {
-    AUTD3_API explicit Gain(const native_methods::Segment segment) : segment(segment) {}
+struct SwapSegmentGain final : Datagram, IntoDatagramTuple<SwapSegmentGain> {
+  AUTD3_API explicit SwapSegmentGain(const native_methods::Segment segment) : segment(segment) {}
 
-    AUTD3_API [[nodiscard]] native_methods::DatagramPtr ptr(const geometry::Geometry&) const override { return AUTDDatagramSwapSegmentGain(segment); }
+  AUTD3_API [[nodiscard]] native_methods::DatagramPtr ptr(const geometry::Geometry&) const override { return AUTDDatagramSwapSegmentGain(segment); }
 
-    native_methods::Segment segment;
-  };
+  native_methods::Segment segment;
+};
 
-  template <transition_mode_v T>
-  struct GainSTM final : Datagram, IntoDatagramTuple<GainSTM<T>> {
-    AUTD3_API explicit GainSTM(const native_methods::Segment segment, const T transition_mode) : segment(segment), transition_mode(transition_mode) {}
+template <transition_mode_v T>
+struct SwapSegmentGainSTM final : Datagram, IntoDatagramTuple<SwapSegmentGainSTM<T>> {
+  AUTD3_API explicit SwapSegmentGainSTM(const native_methods::Segment segment, const T transition_mode)
+      : segment(segment), transition_mode(transition_mode) {}
 
-    AUTD3_API [[nodiscard]] native_methods::DatagramPtr ptr(const geometry::Geometry&) const override {
-      return AUTDDatagramSwapSegmentGainSTM(segment, transition_mode.inner());
-    }
+  AUTD3_API [[nodiscard]] native_methods::DatagramPtr ptr(const geometry::Geometry&) const override {
+    return AUTDDatagramSwapSegmentGainSTM(segment, transition_mode.inner());
+  }
 
-    native_methods::Segment segment;
-    T transition_mode;
-  };
+  native_methods::Segment segment;
+  T transition_mode;
+};
 
-  template <transition_mode_v T>
-  struct FociSTM final : Datagram, IntoDatagramTuple<FociSTM<T>> {
-    AUTD3_API explicit FociSTM(const native_methods::Segment segment, const T transition_mode) : segment(segment), transition_mode(transition_mode) {}
+template <transition_mode_v T>
+struct SwapSegmentFociSTM final : Datagram, IntoDatagramTuple<SwapSegmentFociSTM<T>> {
+  AUTD3_API explicit SwapSegmentFociSTM(const native_methods::Segment segment, const T transition_mode)
+      : segment(segment), transition_mode(transition_mode) {}
 
-    AUTD3_API [[nodiscard]] native_methods::DatagramPtr ptr(const geometry::Geometry&) const override {
-      return AUTDDatagramSwapSegmentFociSTM(segment, transition_mode.inner());
-    }
+  AUTD3_API [[nodiscard]] native_methods::DatagramPtr ptr(const geometry::Geometry&) const override {
+    return AUTDDatagramSwapSegmentFociSTM(segment, transition_mode.inner());
+  }
 
-    native_methods::Segment segment;
-    T transition_mode;
-  };
+  native_methods::Segment segment;
+  T transition_mode;
+};
 
-  template <transition_mode_v T>
-  struct Modulation final : Datagram, IntoDatagramTuple<Modulation<T>> {
-    AUTD3_API explicit Modulation(const native_methods::Segment segment, const T transition_mode)
-        : segment(segment), transition_mode(transition_mode) {}
+template <transition_mode_v T>
+struct SwapSegmentModulation final : Datagram, IntoDatagramTuple<SwapSegmentModulation<T>> {
+  AUTD3_API explicit SwapSegmentModulation(const native_methods::Segment segment, const T transition_mode)
+      : segment(segment), transition_mode(transition_mode) {}
 
-    AUTD3_API [[nodiscard]] native_methods::DatagramPtr ptr(const geometry::Geometry&) const override {
-      return AUTDDatagramSwapSegmentModulation(segment, transition_mode.inner());
-    }
+  AUTD3_API [[nodiscard]] native_methods::DatagramPtr ptr(const geometry::Geometry&) const override {
+    return AUTDDatagramSwapSegmentModulation(segment, transition_mode.inner());
+  }
 
-    native_methods::Segment segment;
-    T transition_mode;
-  };
+  native_methods::Segment segment;
+  T transition_mode;
 };
 
 }  // namespace autd3::driver
