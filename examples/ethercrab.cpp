61bbef7a452a24e153b1ea0ba7566ee3ab7a9feb
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2025-10-02 11:07:24 +0900

    update example

--- examples/ethercrab.cpp
+++ examples/ethercrab.cpp
@@ -18,15 +18,9 @@ int main() try {
   auto autd = autd3::Controller::open(
       std::vector{autd3::AUTD3{.pos = autd3::Point3::origin(), .rot = autd3::Quaternion::Identity()},
                   autd3::AUTD3{.pos = autd3::Point3(autd3::AUTD3::DEVICE_WIDTH, 0.0, 0.0), .rot = autd3::Quaternion::Identity()}},
-      autd3::link::EtherCrab{[](const uint16_t slave, const autd3::link::Status status) {
-                               std::cout << "slave [" << slave << "]: " << status << std::endl;
-                               if (status == autd3::link::Status::Lost()) {
-                                 // You can also wait for the link to recover, without exiting
-                                 // the process
-                                 exit(-1);
-                               }
-                             },
-                             autd3::link::EtherCrabOption{}});
+      autd3::link::EtherCrab{
+          [](const uint16_t idx, const autd3::link::Status status) { std::cout << "Device[" << idx << "]: " << status << std::endl; },
+          autd3::link::EtherCrabOption{}});
   run(autd);
   return 0;
 } catch (std::exception& e) {
